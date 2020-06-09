6620a7f254b4750177a0b5556f75e14d953bd13d
jinGyu_Lee <jinGyu_Lee@DESKTOP-5VJ67IP>
2020-06-09 16:09:55 +0900

    from 107 to 127

---
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,24 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j
+public class SampleController {
+	
+	@RequestMapping("")
+	public void basic() {
+		
+		log.info("basic................");
+	}
+	
+	@RequestMapping(value = "/basic", method = {RequestMethod.GET, RequestMethod.POST})
+	public void basicGet() {
+		log.info("basic get..................");
+	}
+}
